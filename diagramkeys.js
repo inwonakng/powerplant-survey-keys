0e50a3260056f72c0f0aedf9c5369891fac756a8
inwonakng <inwon.kang04@gmail.com>
2021-10-26 22:37:49 -0400

    GOT IT

--- diagramkeys.js
+++ diagramkeys.js
@@ -13,4 +13,4 @@ prompt_texts = {
 2: "A power plant is being planned to be built in either your city or your neighboring city.\nThe power plant would guarantee an increase in everyone in both city\'s (including you) income by boosting the economy.\nBut it also comes with the risk of decreasing the lifespan of people in the city it is built.\nYou are a resident of <b>city  <a id=\'city\'></a></b>."
 }
 
-var icon_list = '<li>The ðŸ‘¥ icon represents the population of the city. </li><li>The $$$ sign represents the income increase. </li><li>The ðŸ’“ icon represents the expected lifespan. </li>'
+var icon_list = '<li>The 👥 icon represents the population of the city. </li><li>The $$$ sign represents the income increase. </li><li>The 💓 icon represents the expected lifespan. </li>'
