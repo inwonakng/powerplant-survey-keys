0afdefcb5c9c8ae78cbe7f90aaab32de316f1d44
inwonakng <inwon.kang04@gmail.com>
2021-10-26 22:12:26 -0400

    first commit

---
+++ diagramkeys.js
@@ -0,0 +1,15 @@
+var imgkeys = {
+    'u_c1_p_c1':  '1EGtezErvlQJI_FfZsDpD4VKbDudYYcVq',
+    'u_c1_p_c2':  '1tuU_Qnjo30J0dgiQQ2pqFrFs0jCTxiDp',
+    'u_c2_p_c1':  '11PfAc8q3XNlG69YYV7lSUw6sdvMRfV5j',
+    'u_c2_p_c2':  '1yMBJxCh3_cCDuWxVPKKSFelJeB-bu20D',
+    'justonecity': '1bPpcurNEKmYPGmSjZt2JZLGxb8PEwcHs'
+    }
+
+prompt_texts = {
+1: "Your city wants to build a new power plant.\nThe power plant would guarantee an increase in everyone\'s income by boosting the economy.\nBut it also comes with the risk of decreasing the lifespan of people in your city.",
+2: "A power plant is being planned to be built in either your city or your neighboring city.\nThe power plant would guarantee an increase in everyone in both city\'s (including you) income by boosting the economy.\nBut it also comes with the risk of decreasing the lifespan of people in the city it is built.\nYou are a resident of <b>city  <a id=\'city\'></a></b>."
+}
+
+var icon_list = '<li>The ðŸ‘¥ icon represents the population of the city. </li><li>The $$$ sign represents the income increase. </li><li>The ðŸ’“ icon represents the expected lifespan. </li>'
+@inwonakng
